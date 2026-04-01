ac78768539a4c243b0f8e009bfcbef93fc5f647c
maelthomas <126707858+maelthomas@users.noreply.github.com>
2026-04-01 19:02:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from gpiozero import Button
 from time import sleep
 
 # On force le pull_down car ton bouton est au 3.3V
-bouton = Button(26, pull_up=False)
+bouton = Button(13, pull_up=False)
 
 print("Test de lecture du bouton (Ctrl+C pour quitter)")
 print("Le bouton doit afficher 0 au repos et 1 quand on appuie.")
